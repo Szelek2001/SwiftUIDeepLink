948300dea4fa37bca4026317f3ffdc54dbfdf525
Rafal Kruszyna <>
2022-09-27 13:35:14 +0200

    init

---
+++ linkbuilder/linkbuilder/linkbuilderApp.swift
@@ -0,0 +1,17 @@
+//
+//  linkbuilderApp.swift
+//  linkbuilder
+//
+//  Created by Axxiome Health on 27/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct linkbuilderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
