3969a337a5794f40a83c746949705ee16aefae4b
Rafal Kruszyna <>
2022-10-17 16:16:07 +0200

    fix error with MyTextEditor

--- linkbuilder/linkbuilder/Base/MyViews/MyTextEditor.swift
+++ linkbuilder/linkbuilder/Base/MyViews/MyTextEditor.swift
@@ -6,11 +6,11 @@
 //
 import SwiftUI
 struct MyTextEditor: View {
-    @State var text: String
+    @State var text: Binding<String>
     var editDisable: Bool
     var frameHeight: CGFloat? = 40
     var body: some View {
-        TextEditor(text: $text)
+        TextEditor(text: text)
             .disabled(editDisable)
             .colorMultiply(editDisable ?  Color(.aokGray1!) : Color(.aokWhite!))
             .cornerRadius(10)
